cfcdf9ce505cab351270d386dd9e76909f392446
Ryan Dixon <rjd323@drexel.edu>
2021-09-16 11:28:01 -0400

    add charge penny pickup

--- entityVariants/registry.lua
+++ entityVariants/registry.lua
@@ -2,5 +2,6 @@ return {
     lostPenny = include("entityVariants/lostPenny"),
     lostKey = include("entityVariants/lostKey"),
     lostBomb = include("entityVariants/lostBomb"),
-    momsFinger = include("entityVariants/momsFinger")
+    momsFinger = include("entityVariants/momsFinger"),
+    chargedPenny = include("entityVariants/chargedPenny")
 }
