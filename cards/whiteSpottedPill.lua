6c70783db7beb445553e547f7f8f30120d7e8725
Ryan Dixon <rjd323@drexel.edu>
2021-10-14 17:20:46 -0400

    add white spotted pill

--- cards/whiteSpottedPill.lua
+++ cards/whiteSpottedPill.lua
@@ -1,13 +1,29 @@
 local helper = include('helper_functions')
 
--- Reroll your oldest passive | reroll all items in room | reroll all of your passives
+-- Shuffle values of coins, keys, and bombs | reroll all items in room | reroll all of your passives
 local Name = "Pills! White Spotted"
 local Tag = "whiteSpottedPill"
 local Id = Isaac.GetCardIdByName(Name)
 local Weight = 1
 
 local function MC_USE_CARD(_, c, p)
-
+    local sfx = lootdeck.sfx
+    local effect = lootdeck.rng:RandomInt(3)
+    local data = p:GetData()
+    if effect == 0 then
+        sfx:Play(SoundEffect.SOUND_THUMBSUP, 1, 0)
+        local coins = p:GetNumCoins()
+        local bombs = p:GetNumBombs()
+        local keys = p:GetNumKeys()
+        p:AddCoins(bombs - coins)
+        p:AddBombs(keys - bombs)
+        p:AddKeys(coins - keys)
+    elseif effect == 1 then
+        helper.SimpleLootCardEffect(p, CollectibleType.COLLECTIBLE_D6, SoundEffect.SOUND_EDEN_GLITCH)
+    else
+        p:PlayExtraAnimation("Glitch")
+        helper.SimpleLootCardEffect(p, CollectibleType.COLLECTIBLE_D4, SoundEffect.SOUND_EDEN_GLITCH)
+    end
 end
 
 return {
