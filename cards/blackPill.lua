e7b642056fbdbc340a6f49cd34f82eade028759a
Ryan Dixon <rjd323@drexel.edu>
2021-10-14 15:51:28 -0400

    add black pill

--- cards/blackPill.lua
+++ cards/blackPill.lua
@@ -7,7 +7,52 @@ local Id = Isaac.GetCardIdByName(Name)
 local Weight = 1
 
 local function MC_USE_CARD(_, c, p)
+    local sfx = lootdeck.sfx
+	local effect = lootdeck.rng:RandomInt(3)
+	local data = p:GetData()
+	if effect == 0 then
+        sfx:Play(SoundEffect.SOUND_DEATH_CARD,1,0)
+        local illegalParents = {
+            EntityType.ENTITY_GEMINI, -- gemini's umbilical cord
+            EntityType.ENTITY_THE_HAUNT, -- the haunt's minions
+            EntityType.ENTITY_FORSAKEN, -- the forsaken's minions
+            EntityType.ENTITY_HERETIC, -- the forsaken's minions
+        }
 
+        helper.ForEachEntityInRoom(function(entity)
+            entity:Kill()
+            local poof = Isaac.Spawn(EntityType.ENTITY_EFFECT, EffectVariant.POOF01, 0, entity.Position, Vector.Zero, p)
+            poof.Color = Color(0,0,0,1,0,0,0)
+        end, nil, nil, nil,
+        function(entity)
+            local npc = entity:ToNPC()
+            return npc and not npc:IsBoss() and npc:IsVulnerableEnemy() and not helper.TableContains(illegalParents, npc.SpawnerType or 0)
+        end)
+
+        helper.ForEachEntityInRoom(function(entity)
+            entity:TakeDamage(80, 0, EntityRef(p), 0)
+            local poof = Isaac.Spawn(EntityType.ENTITY_EFFECT, EffectVariant.POOF01, 0, entity.Position, Vector.Zero, p)
+            poof.Color = Color(0,0,0,1,0,0,0)
+        end, nil, nil, nil,
+        function(entity)
+            local npc = entity:ToNPC()
+            return npc and npc:IsVulnerableEnemy()
+        end)
+	elseif effect == 1 then
+        helper.ForEachEntityInRoom(function(entity)
+            entity:AddConfusion(EntityRef(f), 150, false)
+            local poof = Isaac.Spawn(EntityType.ENTITY_EFFECT, EffectVariant.POOF01, 0, entity.Position, Vector.Zero, p)
+            poof.Color = Color(1,1,1,1,1,1,1)
+        end, nil, nil, nil,
+        function(entity)
+            local npc = entity:ToNPC()
+            return npc and npc:IsVulnerableEnemy()
+        end)
+		sfx:Play(SoundEffect.SOUND_THUMBSUP	,1,0)
+	else
+		helper.TakeSelfDamage(p, 2)
+		sfx:Play(SoundEffect.SOUND_THUMBS_DOWN,1,0)
+	end
 end
 
 return {
