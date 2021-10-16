e4636c370fb50145bfaacfbf2dac0d1b3b3c8338
Ryan Dixon <rjd323@drexel.edu>
2021-10-16 12:35:47 -0400

    added humor to our lives (ahaha fuckkk)

--- entityVariants/doubleStickyNickel.lua
+++ entityVariants/doubleStickyNickel.lua
@@ -32,6 +32,7 @@ local function MC_POST_PICKUP_UPDATE(_, pi)
 end
 
 local function MC_PRE_PICKUP_COLLISION(_, pi, e)
+    print("collided with ?????")
     local p = e:ToPlayer() or 0
     if p ~= 0 then
         local data = pi:GetData()
@@ -47,10 +48,13 @@ local function MC_PRE_PICKUP_COLLISION(_, pi, e)
             return true
         end
     end
-    local ef = e:ToEffect() or 0
-    if ef ~= 0 then
-        if ef.Type == EntityType.ENTITY_EFFECT
-        and ef.Variant == EffectVariant.BOMB_EXPLOSION then
+    local b = e:ToBomb() or 0
+    if b ~= 0 then
+        print("why dot dot its a bomb")
+        local sprite = b:GetSprite()
+        if sprite:IsPlaying("Explode") then
+            print("its EXPLODING!! (this shouldnt run)")
+            Isaac.Spawn(EntityType.ENTITY_PICKUP, PickupVariant.PICKUP_COIN, CoinSubType.COIN_NICKEL, pi.Position, Vector.Zero, pi.Parent)
             Isaac.Spawn(EntityType.ENTITY_PICKUP, PickupVariant.PICKUP_COIN, CoinSubType.COIN_NICKEL, pi.Position, Vector.Zero, pi.Parent)
             pi:Remove()
         end
@@ -65,16 +69,16 @@ local function MC_POST_PICKUP_INIT(_, pi)
     end
 end
 
-local function MC_ENTITY_TAKE_DMG(_, pi, _, flags)
-    print("uouwch")
-    if pi.Variant == Id then
-        if flags & DamageFlag.DAMAGE_EXPLOSION == 0 then
-            Isaac.Spawn(EntityType.ENTITY_PICKUP, PickupVariant.PICKUP_COIN, CoinSubType.COIN_NICKEL, pi.Position, Vector.Zero, pi.Parent)
-            Isaac.Spawn(EntityType.ENTITY_PICKUP, PickupVariant.PICKUP_COIN, CoinSubType.COIN_NICKEL, pi.Position, Vector.Zero, pi.Parent)
-            pi:Remove()
-        end
-    end
-end
+-- local function MC_ENTITY_TAKE_DMG(_, pi, _, flags)
+--     print("uouwch")
+--     if pi.Variant == Id then
+--         if flags & DamageFlag.DAMAGE_EXPLOSION == 0 then
+--             Isaac.Spawn(EntityType.ENTITY_PICKUP, PickupVariant.PICKUP_COIN, CoinSubType.COIN_NICKEL, pi.Position, Vector.Zero, pi.Parent)
+--             Isaac.Spawn(EntityType.ENTITY_PICKUP, PickupVariant.PICKUP_COIN, CoinSubType.COIN_NICKEL, pi.Position, Vector.Zero, pi.Parent)
+--             pi:Remove()
+--         end
+--     end
+-- end
 
 return {
     Name = Name,
@@ -95,11 +99,11 @@ return {
             ModCallbacks.MC_POST_PICKUP_INIT,
             MC_POST_PICKUP_INIT,
             Id
-        },
-        {
-            ModCallbacks.MC_ENTITY_TAKE_DMG,
-            MC_ENTITY_TAKE_DMG,
-            EntityType.ENTITY_PICKUP
-        },
+        }
+        -- {
+        --     ModCallbacks.MC_ENTITY_TAKE_DMG,
+        --     MC_ENTITY_TAKE_DMG,
+        --     EntityType.ENTITY_PICKUP
+        -- },
     }
 }
