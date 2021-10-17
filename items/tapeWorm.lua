4123692243cf4f2bf16dd18ebbb48287ad147594
Noah Richardson <ngregrichardson@gmail.com>
2021-10-17 01:55:55 -0400

    Implement tape worm

--- items/tapeWorm.lua
+++ items/tapeWorm.lua
@@ -5,8 +5,20 @@ local Name = "Tape Worm"
 local Tag = "tapeWorm"
 local Id = Isaac.GetItemIdByName(Name)
 
-local function MC_POST_PEFFECT_UPDATE(_, p)
+local function MC_POST_FIRE_TEAR(_, tear)
+    local p = tear:GetLastParent():ToPlayer()
+    if p:HasCollectible(Id) and helper.PercentageChance(10 * p:GetCollectibleNum(Id), 25) then
+        tear.FallingSpeed = 0
+        tear.FallingAcceleration = -0.1
+        tear:GetData()[Tag] = true
+    end
+end
 
+local function MC_POST_TEAR_UPDATE(_, tear)
+    if tear:GetData()[Tag] and Isaac.GetFrameCount() % 4 == 0 then
+        local creep = Isaac.Spawn(EntityType.ENTITY_EFFECT, EffectVariant.PLAYER_CREEP_WHITE, 0, tear.Position, Vector.Zero, tear)
+        creep:GetSprite().Scale = Vector(1, 1) * 0.3
+    end
 end
 
 return {
@@ -15,8 +27,12 @@ return {
 	Id = Id,
     callbacks = {
         {
-            ModCallbacks.MC_POST_PEFFECT_UPDATE,
-            MC_POST_PEFFECT_UPDATE
+            ModCallbacks.MC_POST_FIRE_TEAR,
+            MC_POST_FIRE_TEAR
+        },
+        {
+            ModCallbacks.MC_POST_TEAR_UPDATE,
+            MC_POST_TEAR_UPDATE
         }
     }
 }
