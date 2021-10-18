117d5a73c3fd31e380386bae9e183c1160e6739e
Ryan Dixon <rjd323@drexel.edu>
2021-10-18 15:53:00 -0400

    add visual/audio cues for get out of jail

--- cards/getOutOfJail.lua
+++ cards/getOutOfJail.lua
@@ -7,8 +7,13 @@ local Id = Isaac.GetCardIdByName(Name)
 local Weight = 1
 
 local function MC_USE_CARD(_, c, p)
-    p:GetData()[Tag] = 5 * 60
-    --p.EntityCollisionClass = EntityCollisionClass.ENTCOLL_PLAYEROBJECTS
+    local data = p:GetData()
+    data[Tag] = 5 * 60
+    data[Tag .. "Color"] = p.Color
+    p.Color = Color(p.Color.R,p.Color.G,p.Color.B,0.5,p.Color.RO,p.Color.GO,p.Color.BO)
+    local poof = Isaac.Spawn(EntityType.ENTITY_EFFECT, EffectVariant.POOF01, 0, p.Position, Vector.Zero, p)
+    poof.Color = Color(1,1,1,1,1,1,1)
+    lootdeck.sfx:Play(SoundEffect.SOUND_MIRROR_ENTER, 1, 0)
 end
 
 local function MC_PRE_PLAYER_COLLISION(_, p, collider)
@@ -23,11 +28,23 @@ end
 local function MC_POST_PLAYER_UPDATE(_, p)
     local data = p:GetData()
     if data[Tag] then
+        local alpha = p.Color.A
         if data[Tag] - 1 > 0 then
             data[Tag] = data[Tag] - 1
+            if data[Tag] % 60 == 0 then
+                alpha = 0.5
+                lootdeck.sfx:Play(SoundEffect.SOUND_SOUL_PICKUP, 1, 0)
+            else
+                alpha = 0.25
+            end
         else
             data[Tag] = nil
+            alpha = 1
+            lootdeck.sfx:Play(SoundEffect.SOUND_MIRROR_EXIT, 1, 0)
+            local poof = Isaac.Spawn(EntityType.ENTITY_EFFECT, EffectVariant.POOF01, 0, p.Position, Vector.Zero, p)
+            poof.Color = Color(1,1,1,1,1,1,1)
         end
+        p.Color = Color(p.Color.R,p.Color.G,p.Color.B,alpha,p.Color.RO,p.Color.GO,p.Color.BO)
     end
 end
 
