e40f34522b37a192d258422239627c0eacc6ec64
Ryan Dixon <rjd323@drexel.edu>
2021-10-19 14:25:57 -0400

    implement lovers costume

--- cards/theLovers.lua
+++ cards/theLovers.lua
@@ -1,4 +1,5 @@
 local helper = include("helper_functions")
+local costumes = include("costumes/registry")
 
 -- Gain two temporary hearts for the room
 local Name = "VI. The Lovers"
@@ -7,7 +8,19 @@ local Id = Isaac.GetCardIdByName(Name)
 local Weight = 1
 
 local function MC_USE_CARD(_, c, p)
+    local data = p:GetData()
     helper.AddTemporaryHealth(p, 4)
+    if not data[Tag] then data[Tag] = true end
+    p:AddNullCostume(costumes.lovers)
+end
+
+local function MC_POST_NEW_ROOM()
+    helper.ForEachPlayer(function(p, data)
+        if data[Tag] then
+            data[Tag] = nil
+            p:TryRemoveNullCostume(costumes.lovers)
+        end
+    end)
 end
 
 return {
@@ -20,6 +33,10 @@ return {
             ModCallbacks.MC_USE_CARD,
             MC_USE_CARD,
             Id
-        }
+        },
+        {
+            ModCallbacks.MC_POST_NEW_ROOM,
+            MC_POST_NEW_ROOM
+        },
     }
 }
