e8e0d6bb112618f58dad624630814238c9f47ab5
Noah Richardson <ngregrichardson@gmail.com>
2021-10-28 23:32:45 -0400

    Resolves #198

--- items/tapeWorm.lua
+++ items/tapeWorm.lua
@@ -9,7 +9,7 @@ local WikiDescription = helper.GenerateEncyclopediaPage("10% chance to fire a Wo
 
 local function MC_POST_FIRE_TEAR(_, tear)
     local p = tear:GetLastParent():ToPlayer()
-    if p:HasCollectible(Id) and helper.PercentageChance(10 * p:GetCollectibleNum(Id), 25) then
+    if p:HasCollectible(Id) and helper.PercentageChance(100) then
         tear.FallingSpeed = 0
         tear.FallingAcceleration = -0.1
         tear:GetData()[Tag] = true
@@ -30,15 +30,22 @@ local function MC_POST_FIRE_TEAR(_, tear)
             end
         end
 
-        sprite:ReplaceSpritesheet(0, string.format("gfx/tears/tapeworm tear %s.png", spriteSheetDirection))
-        sprite:LoadGraphics()
+        if not tear:HasTearFlags(helper.NewTearflag(77)) then
+           sprite:ReplaceSpritesheet(0, string.format("gfx/tears/tapeworm tear %s.png", spriteSheetDirection))
+            sprite:LoadGraphics() 
+        end
     end
 end
 
 local function MC_POST_TEAR_UPDATE(_, tear)
-    if tear:GetData()[Tag] and Isaac.GetFrameCount() % 4 == 0 then
-        local creep = Isaac.Spawn(EntityType.ENTITY_EFFECT, EffectVariant.PLAYER_CREEP_WHITE, 0, tear.Position, Vector.Zero, tear)
-        creep:GetSprite().Scale = Vector(1, 1) * 0.3
+    if tear:GetData()[Tag] then
+        if Isaac.GetFrameCount() % 4 == 0 then
+            local creep = Isaac.Spawn(EntityType.ENTITY_EFFECT, EffectVariant.PLAYER_CREEP_WHITE, 0, tear.Position, Vector.Zero, tear)
+            creep:GetSprite().Scale = Vector(1, 1) * 0.3
+        end
+        if tear.StickTarget then
+            tear.FallingAcceleration = 0.1
+        end
     end
 end
 
