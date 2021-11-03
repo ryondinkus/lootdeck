a35a5e3f2f52a6f83aa517152adc4cd6cd03287c
Ryan Dixon <rjd323@drexel.edu>
2021-11-03 12:20:21 -0400

    new hiroephe

--- cards/theHierophant.lua
+++ cards/theHierophant.lua
@@ -1,13 +1,20 @@
 local helper = include("helper_functions")
 local costumes = include("costumes/registry")
 
+local Names = {
+    en_us = "V. The Hierophant",
+    spa = "V. El Hierofante"
+}
 -- Gives two Holy Mantle effects for the room (negates damage twice with minimal cooldown)
-local Name = "V. The Hierophant"
+local Name = Names.en_us
 local Tag = "theHierophant"
 local Id = Isaac.GetCardIdByName(Name)
 local Weight = 1
-local Description = "Grants a {{Collectible313}} Holy Mantle effect that can absorb two hits for the room"
-local WikiDescription = helper.GenerateEncyclopediaPage("Grants a unique Holy Mantle that can absorb two hits for the duration of the room.")
+local Description = {
+    en_us = "Grants a {{Collectible313}} Holy Mantle effect that can absorb two hits",
+    spa = "Otorga un efecto del {{Collectible313}} Manto Sagrado que puede absorber dos golpes"
+}
+local WikiDescription = helper.GenerateEncyclopediaPage("Grants a unique Holy Mantle that can absorb two hits.")
 
 local function MC_USE_CARD(_, c, p)
     local data = p:GetData()
@@ -15,18 +22,20 @@ local function MC_USE_CARD(_, c, p)
 	p:AddNullCostume(costumes.mantle)
 end
 
-local function MC_ENTITY_TAKE_DMG(_, e)
+local function MC_ENTITY_TAKE_DMG(_, e, damageAmount, damageFlags, damageSource)
     local p = e:ToPlayer()
     local data = p:GetData()
     if data[Tag] and data[Tag] > 0 then
-        helper.HolyMantleEffect(p)
-        data[Tag] = data[Tag] - 1
-		p:TryRemoveNullCostume(costumes.mantle)
-		p:TryRemoveNullCostume(costumes.mantleBroken)
-		if data[Tag] == 1 then
-			p:AddNullCostume(costumes.mantleBroken)
-		end
-        return false
+        if helper.HolyMantleDamage(damageAmount, damageFlags, damageSource) then
+            helper.HolyMantleEffect(p)
+            data[Tag] = data[Tag] - 1
+    		p:TryRemoveNullCostume(costumes.mantle)
+    		p:TryRemoveNullCostume(costumes.mantleBroken)
+    		if data[Tag] == 1 then
+    			p:AddNullCostume(costumes.mantleBroken)
+    		end
+            return false
+        end
     else
         data[Tag] = nil
     end
@@ -34,9 +43,22 @@ end
 
 local function MC_POST_NEW_ROOM()
     helper.ForEachPlayer(function(p, data)
-        data[Tag] = nil
-		p:TryRemoveNullCostume(costumes.mantle)
-		p:TryRemoveNullCostume(costumes.mantleBroken)
+        if data[Tag] then
+            data[Tag .. "KeepCostume"] = true
+        end
+    end)
+end
+
+local function MC_POST_UPDATE()
+    helper.ForEachPlayer(function(p, data)
+        if data[Tag .. "KeepCostume"] then
+            if data[Tag] == 2 then
+                p:AddNullCostume(costumes.mantle)
+            elseif data[Tag] == 1 then
+                p:AddNullCostume(costumes.mantleBroken)
+            end
+            data[Tag .. "KeepCostume"] = false
+        end
     end)
 end
 
@@ -61,6 +83,10 @@ return {
         {
             ModCallbacks.MC_POST_NEW_ROOM,
             MC_POST_NEW_ROOM
-		}
+		},
+        {
+            ModCallbacks.MC_POST_UPDATE,
+            MC_POST_UPDATE
+		},
     }
 }
