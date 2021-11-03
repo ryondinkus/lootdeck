1948b199cf4ce06a107203aeab981f3edbb0b798
Noah Richardson <ngregrichardson@gmail.com>
2021-11-03 00:31:10 -0400

    Fix guppy's hairball

--- items/guppysHairball.lua
+++ items/guppysHairball.lua
@@ -7,7 +7,7 @@ local Id = Isaac.GetItemIdByName(Name)
 local Description = "Every instance of damage taken has a 1/6 chance to be blocked"
 local WikiDescription = helper.GenerateEncyclopediaPage("Every instance of damage taken has a 1/6 chance to be blocked.", "- Additional copies of the passive add an extra 1/6 chance, up to 3/6.")
 
-local function MC_ENTITY_TAKE_DMG(_, e)
+local function MC_ENTITY_TAKE_DMG(_, e, damageAmount, damageFlags, damageSource)
     local p = e:ToPlayer()
     if p:HasCollectible(Id) then
         if helper.PercentageChance(100 / 6 * p:GetCollectibleNum(Id), 50) then
