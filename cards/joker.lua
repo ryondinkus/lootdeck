667ba3c5a21551a6783e810852a27782d642f39b
Noah Richardson <ngregrichardson@gmail.com>
2021-11-17 23:29:42 -0500

    Rework The Joker to use states

--- cards/joker.lua
+++ cards/joker.lua
@@ -17,30 +17,12 @@ local Descriptions = {
 local WikiDescription = helper.GenerateEncyclopediaPage("Spawns a Devil Hand familiar, which steals any item in the room and moves it next to the player.", "- This includes shop items and devil deals", "- This can steal non-shop items trapped behind obstacles, allowing the player to access them.")
 
 local function MC_USE_CARD(_, c, p)
-	local rng = lootdeck.rng
-	local itemsList = {}
+	local devilHand = Isaac.Spawn(EntityType.ENTITY_FAMILIAR, entityVariants.devilHand.Id, 0, p.Position, Vector.Zero, p)
+	devilHand:ClearEntityFlags(EntityFlag.FLAG_APPEAR)
+	devilHand:GetData().player = p
 
-	helper.ForEachEntityInRoom(function(entity)
-		table.insert(itemsList, entity)
-	end,
-	EntityType.ENTITY_PICKUP,
-	PickupVariant.PICKUP_COLLECTIBLE,
-	function(entitySubType) return entitySubType ~= 0 end,
-	function(entity) return not entity:GetData().selected end)
-
-	if #itemsList > 0 then
-		local selectedItem = itemsList[rng:RandomInt(#itemsList)+1]:ToPickup()
-		selectedItem:GetData().selected = true
-		local devilHand = Isaac.Spawn(EntityType.ENTITY_FAMILIAR, entityVariants.devilHand.Id, 0, p.Position, Vector.Zero, p)
-		devilHand:ClearEntityFlags(EntityFlag.FLAG_APPEAR)
-		local poof = Isaac.Spawn(EntityType.ENTITY_EFFECT, EffectVariant.POOF01, 0, p.Position, Vector.Zero, p)
-		poof.Color = Color(0,0,0,1,0,0,0)
-		local handData = devilHand:GetData()
-		handData.target = selectedItem
-		handData.jokerPlayerIndex = p.Index
-	else
-		helper.FuckYou(p)
-	end
+	local poof = Isaac.Spawn(EntityType.ENTITY_EFFECT, EffectVariant.POOF01, 0, p.Position, Vector.Zero, p)
+	poof.Color = Color(0,0,0,1,0,0,0)
 end
 
 return {
