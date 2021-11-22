04d1ccf77b123f6ce103fa3a8d7883b68ea27dc7
basketini-jones <60999059+basketini-jones@users.noreply.github.com>
2021-11-22 16:36:47 -0500

    updated descs for future double effects

--- cards/theFool.lua
+++ cards/theFool.lua
@@ -13,7 +13,7 @@ local Descriptions = {
     en_us = "Triggers the {{Collectible419}} Teleport 2.0 effect, teleporting you to an unvisited room# The room you teleport into will have its doors opened",
     spa = "Activa el efecto de {{Colelctible419}} Teletransporte 2.0, teletrasnportándote a una habitación sin visitar"
 }
-local WikiDescription = helper.GenerateEncyclopediaPage("On use, triggers the Teleport 2.0 effect, which teleports you to an unvisited room with certain priority given to special rooms.", "The room you teleport into will have all of its doors opened, including locked doors.", "Holographic Effect: None")
+local WikiDescription = helper.GenerateEncyclopediaPage("On use, triggers the Teleport 2.0 effect, which teleports you to an unvisited room with certain priority given to special rooms.", "The room you teleport into will have all of its doors opened, including locked doors.", "Holographic Effect: Triggers the Necronomicon effect on room entry, dealing 40 damage to all enemies.")
 
 local function MC_USE_CARD(_, c, p)
 	helper.SimpleLootCardEffect(p, CollectibleType.COLLECTIBLE_TELEPORT_2)
--- cards/theSun.lua
+++ cards/theSun.lua
@@ -15,7 +15,7 @@ local Descriptions = {
     en_us = "If used, defeating the boss of this floor triggers the {{Collectible127}} Forget Me Now effect, restarting the floor# The effect will also trigger on use if the boss of this floor is already defeated# After using, all other instances of the card are removed and cannot be found for the rest of the run",
     spa = "Al usarla, cuando se derrote al jefe se activará el efecto de {{Collectible127}} Olvídame Ya#El efecto también se activará si ya se derrotó al jefe#Tras usarse, cualquier otra copia de la carta se removerá y no podrá volverse a encontrar"
 }
-local WikiDescription = helper.GenerateEncyclopediaPage("After using, defeating the boss of the current floor triggers the Forget Me Now effect, which restarts the floor.", "- The effect will also trigger on use if the current floor's boss is already defeated.", "On use, all other instances of this card are removed and cannot be found for the rest of the run.", "Holographic Effect: None")
+local WikiDescription = helper.GenerateEncyclopediaPage("After using, defeating the boss of the current floor triggers the Forget Me Now effect, which restarts the floor.", "- The effect will also trigger on use if the current floor's boss is already defeated.", "On use, all other instances of this card are removed and cannot be found for the rest of the run.", "Holographic Effect: Grants full mapping on the restarted floor.")
 
 local function MC_USE_CARD(_, c, p)
     lootdeck.f.sunUsed = true
