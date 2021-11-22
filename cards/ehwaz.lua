306462f41c445841e095dfbd576905a75fb13d5d
basketini-jones <60999059+basketini-jones@users.noreply.github.com>
2021-11-22 16:14:29 -0500

    updated certain encyclo descriptions

--- cards/ehwaz.lua
+++ cards/ehwaz.lua
@@ -11,9 +11,9 @@ local Id = Isaac.GetCardIdByName(Name)
 local Weight = 1
 local Descriptions = {
     en_us = "{{Collectible285}} D10 effect on use, rerolling all enemies in the room.# All rerolled enemies will have 50% HP.",
-    spa = "Efecto del {{Collectible285}} D10 al usarse (rerolea a todos los enemigos de la habitación"
+    spa = "Efecto del {{Collectible285}} D10 al usarse (rerolea a todos los enemigos de la habitación)"
 }
-local WikiDescription = helper.GenerateEncyclopediaPage("On use, triggers the D10 effect, rerolling all enemies in the room.", "Holographic Effect: Rerolls all room enemies, then rerolls them again.")
+local WikiDescription = helper.GenerateEncyclopediaPage("On use, triggers the D10 effect, rerolling all enemies in the room.", "All rerolled enemies will have 50% HP.", "Holographic Effect: Rerolls all room enemies, then rerolls them again.")
 
 local function MC_USE_CARD(_, c, p)
 	helper.SimpleLootCardEffect(p, CollectibleType.COLLECTIBLE_D10, SoundEffect.SOUND_LAZARUS_FLIP_DEAD)
--- cards/joker.lua
+++ cards/joker.lua
@@ -14,7 +14,7 @@ local Descriptions = {
 	en_us = "Spawns a Devil Hand familiar which follows the player# On room entry, Devil Hand steals any item in the room (shop items included) and brings it to the player# If the item is taken before the Devil Hand can grab it, it will get angry and punch the ground, creating a shockwave.",
 	spa = "Genera una mano del Diablo familiar que robará cualquier objeto de la sala (incluyendo tiendas) y se lo entregará al jugador#Si no hay objetos en la sala, el efecto falla"
 }
-local WikiDescription = helper.GenerateEncyclopediaPage("Spawns a Devil Hand familiar, which steals any item in the room and moves it next to the player.", "- This includes shop items and devil deals", "- This can steal non-shop items trapped behind obstacles, allowing the player to access them.", "Holographic Effect: Spawns two Devil Hands.")
+local WikiDescription = helper.GenerateEncyclopediaPage("Spawns a Devil Hand familiar.", "On room entry, Devil Hand steals any item in the room and moves it next to the player.", "- This includes shop items and devil deals", "- This can steal non-shop items trapped behind obstacles, allowing the player to access them.", "- If the player takes the item before Devil Hand can grab it, Devil Hand will get angry and punch the ground, creating a shockwave that can damage the player.", "Holographic Effect: Spawns two Devil Hands.")
 
 local function MC_USE_CARD(_, c, p)
 	local devilHand = Isaac.Spawn(EntityType.ENTITY_FAMILIAR, entityVariants.devilHand.Id, 0, p.Position, Vector.Zero, p)
