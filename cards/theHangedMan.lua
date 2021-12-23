327bc24d5e205de7331e6b883be90c95e35b85e7
basketini-jones <60999059+basketini-jones@users.noreply.github.com>
2021-12-22 21:34:12 -0500

    changed hanged man effect

--- cards/theHangedMan.lua
+++ cards/theHangedMan.lua
@@ -17,38 +17,21 @@ local Descriptions = {
 local WikiDescription = helper.GenerateEncyclopediaPage("Grants the Magneto effect for the room, which causes pickups to be drawn toward your position.", "Holographic Effect: Fills in all holes in the current room.")
 
 local function MC_USE_CARD(_, c, p, f, shouldDouble)
-    local data = p:GetData().lootdeck
-    local magnetoConfig = Isaac.GetItemConfig():GetCollectible(CollectibleType.COLLECTIBLE_MAGNETO)
-    p:AddCollectible(CollectibleType.COLLECTIBLE_MAGNETO, 0, false)
-    if not data.hangedMan then data.hangedMan = 1
-    else data.hangedMan = data.hangedMan + 1 end
-    if data.hangedMan >= p:GetCollectibleNum(CollectibleType.COLLECTIBLE_MAGNETO) then
-        p:RemoveCostume(magnetoConfig)
-    end
-	p:AddNullCostume(costumes.hangedMan)
-	if shouldDouble then
-		local room = Game():GetRoom()
-		for i=0,room:GetGridSize() do
-			local gridEntity = room:GetGridEntity(i)
-			if gridEntity and gridEntity:ToPit() then
-				gridEntity:ToPit():MakeBridge(gridEntity)
-                Isaac.Spawn(EntityType.ENTITY_EFFECT, EffectVariant.POOF01, 0, gridEntity.Position, Vector.Zero, nil)
+	local room = Game():GetRoom()
+	for i=0,room:GetGridSize() do
+		local gridEntity = room:GetGridEntity(i)
+		if gridEntity and gridEntity:ToPit() then
+			gridEntity:ToPit():MakeBridge(gridEntity)
+			Isaac.Spawn(EntityType.ENTITY_EFFECT, EffectVariant.POOF01, 0, gridEntity.Position, Vector.Zero, nil)
+		end
+		if shouldDouble then
+			if gridEntity then
+				gridEntity:Destroy()
 			end
 		end
-        lootdeck.sfx:Play(SoundEffect.SOUND_ROCK_CRUMBLE)
 	end
-end
-
-local function MC_POST_NEW_ROOM()
-    helper.ForEachPlayer(function(p, data)
-        if data.hangedMan then
-            for j=1,data.hangedMan do
-				p:RemoveCollectible(CollectibleType.COLLECTIBLE_MAGNETO)
-            end
-			p:TryRemoveNullCostume(costumes.hangedMan)
-            data.hangedMan = nil
-        end
-    end)
+	lootdeck.sfx:Play(SoundEffect.SOUND_ROCK_CRUMBLE)
+	Game():ShakeScreen(15)
 end
 
 return {
@@ -64,10 +47,6 @@ return {
             ModCallbacks.MC_USE_CARD,
             MC_USE_CARD,
             Id
-        },
-        {
-            ModCallbacks.MC_POST_NEW_ROOM,
-            MC_POST_NEW_ROOM
         }
     }
 }
