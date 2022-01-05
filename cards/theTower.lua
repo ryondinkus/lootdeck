bac0abd0e564cb3bb8c0b68cab14028be3b3c490
Noah Richardson <ngregrichardson@gmail.com>
2022-01-05 16:40:52 -0500

    Fix the tower I thinks

--- cards/theTower.lua
+++ cards/theTower.lua
@@ -15,8 +15,6 @@ local Descriptions = {
 }
 local WikiDescription = helper.GenerateEncyclopediaPage("On use, spawns an explosion on every enemy in the room, dealing 40 damage to any enemy in the explosion.", "After exploding on all enemies, it will explode on the player.", "Holographic Effect: Spawns explosions two at a time.")
 
-local chosenTag = Tag.."Chosen"
-
 local function MC_USE_CARD(_, c, p, f, shouldDouble)
     local data = p:GetData().lootdeck
 	data[Tag] = 1
@@ -42,17 +40,22 @@ end
 local function MC_POST_PEFFECT_UPDATE(_, p)
 	local rng = p:GetCardRNG(Id)
     local numberOfEnemies = #helper.ListEnemiesInRoom(true, function(_, eData) return eData[Tag] end) + 1
-    helper.StaggerSpawn(Tag, p, 7, numberOfEnemies, function(counterName)
+    helper.StaggerSpawn(Tag, p, 7, numberOfEnemies, function(counterTag, previousResult)
+
+		if not previousResult then
+			previousResult = 1
+		end
+
 		local data = p:GetData().lootdeck
-		if data[counterName] > numberOfEnemies then
-			data[counterName] = numberOfEnemies
+		if data[counterTag] > numberOfEnemies then
+			data[counterTag] = numberOfEnemies
 		end
 		local target
 
-		if data[counterName] == 1 then
+		if data[counterTag] == 1 then
 			target = p
 		else
-			local enemy = helper.GetRandomEnemy(rng, Tag, function(_, eData) return eData[Tag] and not eData[chosenTag] end)
+			local enemy = helper.GetRandomEnemy(rng, nil, function(_, eData) return eData[Tag] end)
 			if enemy then
 				target = enemy
 				enemy:GetData()[Tag] = nil
@@ -60,11 +63,19 @@ local function MC_POST_PEFFECT_UPDATE(_, p)
 				target = p
 			end
 		end
-		Isaac.Explode(target.Position, nil, 40)
-		data[counterName] = data[counterName] - 1
+		
+		if target ~= p or previousResult ~= data[Tag] then
+			Isaac.Explode(target.Position, nil, 40)
+		end
+		data[counterTag] = data[counterTag] - 1
+
+		return previousResult + 1
 	end,
 	function()
-		helper.ClearChosenEnemies(Tag)
+		local enemies = helper.ListEnemiesInRoom()
+		for _, enemy in ipairs(enemies) do
+			enemy:GetData()[Tag] = true
+		end
 	end,
 	true)
 end
